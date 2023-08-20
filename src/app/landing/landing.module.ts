3e006ecba33085f983736d729de9b677b707b680
Oskar Engholm <oskar@gigital.com>
2023-08-20 17:11:13 +0200

    fix link

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 import { LandingAuthFormComponent } from './components/landing-auth-form/landing-auth-form.component';
 import { LandingLoggedInComponent } from './components/landing-logged-in/landing-logged-in.component';
 import { LandingLoggedOutComponent } from './components/landing-logged-out/landing-logged-out.component';
@@ -8,6 +9,6 @@ import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [LandingComponent, LandingLoggedInComponent, LandingLoggedOutComponent, LandingAuthFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
 })
 export class LandingModule {}
